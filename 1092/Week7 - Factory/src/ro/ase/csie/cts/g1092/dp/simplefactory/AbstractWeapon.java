b804961a79a8079d7c205bd2b50ef4f1069d63eb
Catalin <boja.catalin@hotmail.com>
2021-04-06 11:04:22 +0300

    Factory Method Implementation

--- 1092/Week7 - Factory/src/ro/ase/csie/cts/g1092/dp/simplefactory/AbstractWeapon.java
+++ 1092/Week7 - Factory/src/ro/ase/csie/cts/g1092/dp/simplefactory/AbstractWeapon.java
@@ -1,8 +1,8 @@
 package ro.ase.csie.cts.g1092.dp.simplefactory;
 
 public abstract class AbstractWeapon {
-	int powerLevel;
-	String description;
+	protected int powerLevel;
+	protected String description;
 	
 	public abstract void pewPew();
 }
